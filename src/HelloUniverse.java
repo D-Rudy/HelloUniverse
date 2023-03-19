beb6baef162e21e38c6c68b38b04011a83ccf72a
D-Rudy <duriez.ry@gmail.com>
2023-03-19 19:02:00 +0100

    variables

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,5 +1,8 @@
 public class HelloUniverse {
     public static void main(String... args) {
         System.out.println("Hello Universe!");
+        int planete = 8;
+        System.out.println("Aux dernières nouvelles, le nombre de planètes dans le système solaire est de " + planete +
+                " .\nIl y a quelques années, elles étaient au nombre de " + (planete + 1) + " .");
     }
 }
