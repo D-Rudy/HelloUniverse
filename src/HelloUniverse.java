8043c5a76c4eacf7c7c8d815c589581cfc0fd4cb
D-Rudy <duriez.ry@gmail.com>
2023-03-19 19:20:05 +0100

    chaine de caractères

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,8 +1,12 @@
 public class HelloUniverse {
     public static void main(String... args) {
         System.out.println("Hello Universe!");
-        int planete = 8;
-        System.out.println("Aux dernières nouvelles, le nombre de planètes dans le système solaire est de " + planete +
-                " .\nIl y a quelques années, elles étaient au nombre de " + (planete + 1) + " .");
+        int nbPlanete = 8;
+        String maChaine = " Aux dernières nouvelles, le nombre de planètes dans le système solaire est de ";
+        System.out.println( maChaine + nbPlanete +" .");
+        nbPlanete = 9;
+        maChaine =" Il y a quelques années, elles étaient au nombre de " ;
+        System.out.println( maChaine + nbPlanete +" .");
     }
+
 }
