78a83c5be1e73b10b62eba744ddd4d4051458eac
D-Rudy <D-Rudy@users.noreply.github.com>
2023-04-17 18:28:30 +0200

    Structure conditionnelles (if/else)

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,14 +1,13 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        short annee = 2005;
+        short annee = 2021;
         String maChaine = "En %d, les planètes du système solaire étaient au nombre de %d";
         System.out.println("Hello Universe!");
         int nbPlanete = 0;
 
         if (annee >= 2006) {
             nbPlanete = 8;
-        }
-        if (annee < 2006) {
+        }else{
             nbPlanete = 9;
         }
         System.out.printf(maChaine, annee, nbPlanete);
