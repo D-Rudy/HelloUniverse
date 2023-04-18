5e606d6487532425936b8b8bb14b2f282e4e1dc4
D-Rudy <D-Rudy@users.noreply.github.com>
2023-04-18 17:04:21 +0200

    instanciation et affichage d'un objet avec ses parametres laisser par défaut.

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,24 +1,50 @@
 public class HelloUniverse {
     public static void main(String... args) {
 
-        int nbPlanete = 7;
-        while (nbPlanete <= 9) {
-            switch (nbPlanete) {
-                case 7:
-                    System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes. ");
-                    break;
-                case 8:
-                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006. ");
-                    break;
-                case 9:
-                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8. ");
-                    break;
-                default:
-                    System.out.println("Le programme ne peut pas fournir de résultat pour " + nbPlanete);
-                    break;
-            }
-            nbPlanete++;
-        }
+
+        Planet mercure = new Planet();
+        mercure.nom = "Mercure";
+        mercure.matiere = "tellurique";
+        mercure.diametre = 4880l;
+
+        Planet venus = new Planet();
+        venus.nom = "Venus";
+        venus.matiere = "tellurique";
+        venus.diametre = 12100l;
+
+        Planet terre = new Planet();
+        terre.nom = "Terre";
+        terre.matiere = "tellurique";
+        terre.diametre = 12750l;
+
+        Planet mars = new Planet();
+        mars.nom = "Mars";
+        mars.matiere = "tellurique";
+        mars.diametre = 6792l;
+
+        Planet jupiter = new Planet();
+        jupiter.nom = "Jupiter";
+        jupiter.matiere = "gazeuse";
+        jupiter.diametre = 142984l;
+
+        Planet saturne = new Planet();
+        saturne.nom = "Saturne";
+        saturne.matiere = "gazeuse";
+        saturne.diametre = 120536l;
+
+        Planet uranus = new Planet();
+        uranus.nom = "Uranus";
+        uranus.matiere = "gazeuse";
+        uranus.diametre = 51118l;
+
+        Planet neptune = new Planet();
+        neptune.nom = "Neptune";
+        neptune.matiere = "gazeuse";
+        neptune.diametre = 49528l;
+
+        Planet planetX = new Planet();
+        System.out.println(planetX.nom + " est une planète " + planetX.matiere + " de " + planetX.diametre + " km de diamètre");
+        System.out.println(jupiter.nom + " est une planète " + jupiter.matiere + " de " + jupiter.diametre + " km de diamètre");
     }
 }
 
