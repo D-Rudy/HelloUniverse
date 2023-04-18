6c9dc7c9986bada7ced6ae61f69c358b026a5f2f
D-Rudy <D-Rudy@users.noreply.github.com>
2023-04-18 07:00:27 +0200

    Structure conditionnelles imbriquée (if/else-if/else) (correction)

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,25 +1,26 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        short annee = 2021;
+        short annee = 1805;
         String maChaine = "En %d, les planètes du système solaire étaient au nombre de %d";
         System.out.println("Hello Universe!");
         int nbPlanete;
 
-        if (annee >= 1600 && annee <= 2020) {
-            if (annee >= 1600 && annee < 1700) {
+        if (annee < 1600 || annee > 2020) {
+            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d", annee);
+        } else {
+            if (annee < 1700) {
                 nbPlanete = 7;
-            } else if (annee >= 1700 && annee < 1800) {
+            } else if (annee < 1800) {
                 nbPlanete = 8;
-            } else if (annee >= 1800 && annee < 2006) {
+            } else if (annee < 2006) {
                 nbPlanete = 9;
             } else {
                 nbPlanete = 8;
             }
             System.out.printf(maChaine, annee, nbPlanete);
-        } else {
-            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d", annee);
         }
 
     }
 
 }
+
