1679af4343b5092fc31af6a06de07e227a029118
D-Rudy <D-Rudy@users.noreply.github.com>
2023-04-18 17:07:44 +0200

    instanciation et affichage d'un objet avec ses parametres laisser par défaut.

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -5,44 +5,45 @@ public class HelloUniverse {
         Planet mercure = new Planet();
         mercure.nom = "Mercure";
         mercure.matiere = "tellurique";
-        mercure.diametre = 4880l;
+        mercure.diametre = 4880L;
 
         Planet venus = new Planet();
         venus.nom = "Venus";
         venus.matiere = "tellurique";
-        venus.diametre = 12100l;
+        venus.diametre = 12100L;
 
         Planet terre = new Planet();
         terre.nom = "Terre";
         terre.matiere = "tellurique";
-        terre.diametre = 12750l;
+        terre.diametre = 12750L;
 
         Planet mars = new Planet();
         mars.nom = "Mars";
         mars.matiere = "tellurique";
-        mars.diametre = 6792l;
+        mars.diametre = 6792L;
 
         Planet jupiter = new Planet();
         jupiter.nom = "Jupiter";
         jupiter.matiere = "gazeuse";
-        jupiter.diametre = 142984l;
+        jupiter.diametre = 142984L;
 
         Planet saturne = new Planet();
         saturne.nom = "Saturne";
         saturne.matiere = "gazeuse";
-        saturne.diametre = 120536l;
+        saturne.diametre = 120536L;
 
         Planet uranus = new Planet();
         uranus.nom = "Uranus";
         uranus.matiere = "gazeuse";
-        uranus.diametre = 51118l;
+        uranus.diametre = 51118L;
 
         Planet neptune = new Planet();
         neptune.nom = "Neptune";
         neptune.matiere = "gazeuse";
-        neptune.diametre = 49528l;
+        neptune.diametre = 49528L;
 
         Planet planetX = new Planet();
+
         System.out.println(planetX.nom + " est une planète " + planetX.matiere + " de " + planetX.diametre + " km de diamètre");
         System.out.println(jupiter.nom + " est une planète " + jupiter.matiere + " de " + jupiter.diametre + " km de diamètre");
     }
