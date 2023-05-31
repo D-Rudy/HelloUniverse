5b97405d526cce6dbe2a8ec5c485d896ac026d8d
D-Rudy <duriez.ry@gmail.com>
2023-05-31 17:08:30 +0200

    Utilisation du clavier , importation de la classe clavier

--- src/helloUniverse/HelloUniverse.java
+++ src/helloUniverse/HelloUniverse.java
@@ -1,5 +1,7 @@
 package helloUniverse;
 
+import java.util.Scanner;
+
 public class HelloUniverse {
     public static void main(String... args) {
 
@@ -25,55 +27,85 @@ public class HelloUniverse {
         uranus.atmosphere = atmosphereUranus;
         PlaneteGazeuse neptune = new PlaneteGazeuse("Neptune");
         neptune.diametre = 49528;
-
-        System.out.println("la forme d'une planète est " + Planet.forme);
-        System.out.println("la forme de " + mars.nom + " est " + Planet.forme);
-
-        System.out.println(Planet.expension(10.5));
-        System.out.println(Planet.expension(14.2));
-        System.out.println("L'Homme à découvert " + Planet.nbPlaneteDecouvertes + " planètes.");
-
-        VaisseauDeGuerre chasseur = new VaisseauDeGuerre("CHASSEUR");
+        Vaisseau chasseur = new VaisseauDeGuerre("CHASSEUR");
+        chasseur.nombrePassagers = 3;
         chasseur.blindage = 156;
         chasseur.resistanceBouclier = 2;
+        Vaisseau fregate = new VaisseauDeGuerre("FREGATE");
+        fregate.nombrePassagers = 100;
+        fregate.blindage = 542;
+        fregate.resistanceBouclier = 50;
+        Vaisseau croiseur = new VaisseauDeGuerre("CROISEUR");
+        croiseur.nombrePassagers = 35;
+        croiseur.blindage = 851;
+        croiseur.resistanceBouclier = 25;
 
+        Vaisseau cargo = new VaisseauCivil("CARGO");
+        cargo.nombrePassagers = 10000;
+        cargo.blindage = 1520;
+        cargo.resistanceBouclier = 20;
         Vaisseau vaisseauMonde = new VaisseauCivil("VAISSEAU-MONDE");
+        vaisseauMonde.nombrePassagers = 10000;
         vaisseauMonde.blindage = 4784;
         vaisseauMonde.resistanceBouclier = 30;
 
-        vaisseauMonde.activerBouclier();
 
-        chasseur.activerBouclier();
+        System.out.println("Quel vaisseau souhaitez-vous utiliser ?");
+        Scanner input = new Scanner(System.in);
+        String vaisseauChoisi = input.nextLine();
+        Vaisseau vaisseau = null;
+        switch (vaisseauChoisi) {
 
-        chasseur.attaque(vaisseauMonde, "Laser photoniques", 3);
+            case "CHASSEUR":
+                System.out.println("Vous avez choisi le vaisseau de type CHASSEUR");
+                vaisseau = chasseur;
+                break;
+            case "FREGATE":
+                System.out.println("Vous avez choisi le vaisseau de type FREGATE");
+                vaisseau = fregate;
+                break;
+            case "CROISEUR":
+                System.out.println("Vous avez choisi le vaisseau de type CROISEUR");
+                vaisseau = croiseur;
+                break;
+            case "CARGO":
+                System.out.println("Vous avez choisi le vaisseau de type CARGO");
+                vaisseau = cargo;
+                break;
+            case "VAISSEAU-MONDE":
+                System.out.println("Vous avez choisi le vaisseau de type VAISSEAU-MONDE");
+                vaisseau = vaisseauMonde;
+                break;
+        }
 
-        vaisseauMonde.desactiverBouclier();
+        System.out.println("Sur quelle planète téllurique souhaitez-vous vous poser ?");
+        String planeteChoisie = input.nextLine();
+        PlaneteTellurique planete = null;
+        switch (planeteChoisie) {
+            case "Mercure":
+                System.out.println("Vous avez choisi la planète Mercure");
+                planete = mercure;
+                break;
+            case "Venus":
+                System.out.println("Vous avez choisi la planète Venus");
+                planete = venus;
+                break;
+            case "Terre":
+                System.out.println("Vous avez choisi la planète Terre");
+                planete = terre;
+                break;
+            case "Mars":
+                System.out.println("Vous avez choisi la planète Mars");
+                planete = mars;
+                break;
+        }
+        System.out.println("Quel tonnage souhaitez-vous embarquer ?");
+        int tonnageChoisi = input.nextInt();
 
-        System.out.println("Durée de protection résiduelle du bouclier: " + vaisseauMonde.resistanceBouclier + " secondes.");
-        System.out.println("Blindage restant: " + vaisseauMonde.blindage + " points.");
+        System.out.println(vaisseau.emporterCargaison(tonnageChoisi) + "tonnes rejetées");
+        planete.accueillirVaisseau(vaisseau);
 
-        mars.accueillirVaisseau(vaisseauMonde);
-        mars.accueillirVaisseau(chasseur);
 
-        Vaisseau chasseurSurTerre = new VaisseauDeGuerre("CHASSEUR");
-        terre.accueillirVaisseau(chasseurSurTerre);
-        System.out.println(" le chasseur à rejeté " + chasseurSurTerre.emporterCargaison(20) + " tonnes");
-
-        Vaisseau fregateSurTerre = new VaisseauDeGuerre("FREGATE");
-        fregateSurTerre.nombrePassagers = 100;
-        terre.accueillirVaisseau(fregateSurTerre);
-        System.out.println(" la fregate à rejeté " + fregateSurTerre.emporterCargaison(45) + " tonnes");
-        System.out.println(" la fregate à rejeté " + fregateSurTerre.emporterCargaison(12) + " tonnes");
-
-        Vaisseau fregateSurTerre2 = new VaisseauDeGuerre("FREGATE");
-        fregateSurTerre2.nombrePassagers = 14;
-        terre.accueillirVaisseau(fregateSurTerre2);
-        System.out.println(" la fregate à rejeté " + fregateSurTerre2.emporterCargaison(30) + " tonnes");
-
-        Vaisseau vaisseauMondeSurTerre = new VaisseauCivil("VAISSEAU-MONDE");
-        terre.accueillirVaisseau(vaisseauMondeSurTerre);
-        System.out.println(" le vaisseau monde à rejeté " + vaisseauMondeSurTerre.emporterCargaison(1560) + " tonnes");
-        System.out.println(" le vaisseau monde à rejeté " + vaisseauMondeSurTerre.emporterCargaison(600) + " tonnes");
     }
 }
 
